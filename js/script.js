9b4af52d8d0053bd2d609115e9d6180a4c768025
Kelvin Washiko <washikokevv27@gmail.com>
2020-08-21 17:06:30 +0300

    added alert

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 var dateOfBirth = prompt ('please enter birth date');
 var monthOfBirth = prompt ('please enter birth month');
 var yearOfBirth = prompt ('please enter birth year');
+alert ('My Birthday is ' + [dateOfBirth] + '/' + [monthOfBirth] + '/' + [yearOfBirth]);
