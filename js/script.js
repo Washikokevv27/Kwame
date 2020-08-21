009c39d9f59b72619f45f6cf35ae6c5ddc0aa183
Kelvin Washiko <washikokevv27@gmail.com>
2020-08-21 17:43:25 +0300

    added centery

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ var dateOfBirth = prompt ('please enter birth date');
 var monthOfBirth = prompt ('please enter birth month');
 var yearOfBirth = prompt ('please enter birth year');
 alert ('My Birthday is ' + [dateOfBirth] + '/' + [monthOfBirth] + '/' + [yearOfBirth]);
+
+var century = parseInt(yearOfBirth[0]+yearOfBirth[1]);
+alert(century);
