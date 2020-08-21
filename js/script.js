385aa0ebc9425138cafde1b6bfba9e5adbcf0075
Kelvin Washiko <washikokevv27@gmail.com>
2020-08-21 18:06:29 +0300

    added a line

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@ var yearOfBirth = prompt ('please enter birth year');
 alert ('My Birthday is ' + [dateOfBirth] + '/' + [monthOfBirth] + '/' + [yearOfBirth]);
 
 var century = parseInt(yearOfBirth[0]+yearOfBirth[1]);
-alert(century);
+var year = parseInt(yearOfBirth[2]+yearOfBirth[3]);
+var month = parseInt(monthOfBirth);
+var date = parseInt(dateOfBirth);
+alert([century] + [year]); 
+
+Day  (d) = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) %7
