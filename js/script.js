89456f62d7addf5b11f15ccf5fe3eba97daa5f47
Kelvin Washiko <washikokevv27@gmail.com>
2020-08-22 15:46:53 +0300

    adding days of the week

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var dateOfBirth = prompt ('please enter birth date');
 var monthOfBirth = prompt ('please enter birth month');
 var yearOfBirth = prompt ('please enter birth year');
+var genderAtBirth = prompt ('please enter gender assigned at birth')
 alert ('My Birthday is ' + [dateOfBirth] + '/' + [monthOfBirth] + '/' + [yearOfBirth]);
 
 var century = parseInt(yearOfBirth[0]+yearOfBirth[1]);
@@ -9,8 +10,16 @@ var month = parseInt(monthOfBirth);
 var date = parseInt(dateOfBirth);
 alert([century] + [year]); 
 
-function findWeekDay(w) {
-    day  = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) %7  
-    return(day)
-}
-alert(findWeekDay())
+function findWeekDay(d){
+    dayOfBirth = ( ( (([century]/4) -2)*([century]-1)) + ((5*[year]/4) ) + ((26*([month]+1)/10)) + [date] )%7;
+    return(dayOfBirth);
+ }
+ alert(findWeekDay());
+ var nDay = Math.trunc(dayOfBirth);
+ alert(nDay)
+ if (nDay>=3) {
+     nDayBorn= nDay -2;
+ }
+ else {
+     nDayBorn=nDay;
+ }; 
